a6aefcc6cc62f5520361928d4603555fc5a9d61f
BilalAcil <bilal-acil@web.de>
2026-01-09 09:39:34 +0100

    JSDoc level 1

--- El-Pollo-Loco/js/levels/level1.js
+++ El-Pollo-Loco/js/levels/level1.js
@@ -1,13 +1,28 @@
+/**
+ * Erzeugt Wolken über die gesamte Levelbreite.
+ *
+ * @function generateClouds
+ * @param {number} levelWidth - Breite des Levels in Pixeln.
+ * @param {number} [spacing=500] - Abstand zwischen Wolken (je kleiner, desto mehr Wolken).
+ * @returns {Cloud[]} Array aus Cloud-Objekten mit leicht zufälligem X-Offset.
+ */
 function generateClouds(levelWidth, spacing = 500) {
+  /** @type {Cloud[]} */
   let clouds = [];
+
   for (let x = 0; x < levelWidth; x += spacing) {
     let offsetX = x + Math.random() * 200; // Etwas zufälliger
     clouds.push(new Cloud(offsetX));
   }
+
   return clouds;
 }
 
-
+/**
+ * Level 1 – enthält Gegner, Wolken und Hintergrund-Objekte.
+ *
+ * @type {Level}
+ */
 const level1 = new Level(
   [
     new Chicken(),
@@ -28,44 +43,46 @@ const level1 = new Level(
   ],
   generateClouds(10000),
 
-  [new BackgroundObject('img/5_background/layers/air.png', -720),
-  new BackgroundObject('img/5_background/layers/3_third_layer/2.png', -720),
-  new BackgroundObject('img/5_background/layers/2_second_layer/2.png', -720),
-  new BackgroundObject('img/5_background/layers/1_first_layer/2.png', -720),
+  [
+    new BackgroundObject('img/5_background/layers/air.png', -720),
+    new BackgroundObject('img/5_background/layers/3_third_layer/2.png', -720),
+    new BackgroundObject('img/5_background/layers/2_second_layer/2.png', -720),
+    new BackgroundObject('img/5_background/layers/1_first_layer/2.png', -720),
 
-  new BackgroundObject('img/5_background/layers/air.png', 0),
-  new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0),
-  new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0),
-  new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0),
+    new BackgroundObject('img/5_background/layers/air.png', 0),
+    new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0),
+    new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0),
+    new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0),
 
-  new BackgroundObject('img/5_background/layers/air.png', 720),
-  new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 720),
-  new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 720),
-  new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 720),
+    new BackgroundObject('img/5_background/layers/air.png', 720),
+    new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 720),
+    new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 720),
+    new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 720),
 
-  new BackgroundObject('img/5_background/layers/air.png', 720 * 2),
-  new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 720 * 2),
-  new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 720 * 2),
-  new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 720 * 2),
+    new BackgroundObject('img/5_background/layers/air.png', 720 * 2),
+    new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 720 * 2),
+    new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 720 * 2),
+    new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 720 * 2),
 
-  new BackgroundObject('img/5_background/layers/air.png', 720 * 3),
-  new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 720 * 3),
-  new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 720 * 3),
-  new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 720 * 3),
+    new BackgroundObject('img/5_background/layers/air.png', 720 * 3),
+    new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 720 * 3),
+    new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 720 * 3),
+    new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 720 * 3),
 
-  new BackgroundObject('img/5_background/layers/air.png', 720 * 4),
-  new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 720 * 4),
-  new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 720 * 4),
-  new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 720 * 4),
+    new BackgroundObject('img/5_background/layers/air.png', 720 * 4),
+    new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 720 * 4),
+    new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 720 * 4),
+    new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 720 * 4),
 
-  new BackgroundObject('img/5_background/layers/air.png', 720 * 5),
-  new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 720 * 5),
-  new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 720 * 5),
-  new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 720 * 5),
+    new BackgroundObject('img/5_background/layers/air.png', 720 * 5),
+    new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 720 * 5),
+    new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 720 * 5),
+    new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 720 * 5),
 
-  new BackgroundObject('img/5_background/layers/air.png', 720 * 6),
-  new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 720 * 6),
-  new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 720 * 6),
-  new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 720 * 6)],
+    new BackgroundObject('img/5_background/layers/air.png', 720 * 6),
+    new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 720 * 6),
+    new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 720 * 6),
+    new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 720 * 6)
+  ],
   4550
 );
